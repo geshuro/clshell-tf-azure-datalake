b7bd8d09666ff75ea4063fe0abaaf48b054ee904
imendozah <imendozak24@gmail.com>
2021-07-25 21:07:56 -0500

    change azurerm >=2.46.0

--- app/versions.tf
+++ app/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.29.0"
+      version = ">=2.46.0"
     }
     databricks = {
       source  = "databrickslabs/databricks"
